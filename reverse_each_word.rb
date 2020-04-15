89ec258dd013a05c9b62daab24a5892ed196cf31
Danny Padron <danny.padron09@gmail.com>
2020-04-15 23:24:48 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(sentence)
-  sentence_array = sentence.split(" ")
+  sentence_array = sentence.split
   new_sentence = []
   sentence_array.each do |word|
     new_sentence << word.reverse
